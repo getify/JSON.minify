533576f4844cf20fd7d29714837f32090ec8c768
Johannes Wilm <mail@johanneswilm.org>
2017-09-19 10:33:55 +0200

    add wheel info

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 setup(
     # Metadata
     name="JSON_minify",
-    version="0.2.0",
+    version="0.2.1",
     include_package_data=True,
     license='MIT License',
     description=(
