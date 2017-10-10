f371ea418136dcb77a2ffa5bfd63d37d6895357d
Pradyun Gedam <pradyunsg@gmail.com>
2017-10-10 15:18:08 +0530

    Bump Version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except OSError:
 setup(
     # Metadata
     name="JSON_minify",
-    version="0.2.0",
+    version="0.3.0",
     description=(
         "A simple script to minify valid JSON, containing C/C++ style comments"
     ),
