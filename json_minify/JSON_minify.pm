1a477033e0d960e6b21d4ba48ee8044f4f883dc1
Rémi Cohen-Scali <remi@jayacode.fr>
2018-01-08 17:02:42 +0100

    Algo implemented with traces commented out
    firsts tests ok

--- json_minify/JSON_minify.pm
+++ json_minify/JSON_minify.pm
@@ -1,4 +1,4 @@
-#/usr/bin/perl
+#!/usr/bin/perl
 
 package JSON_minify;
 
@@ -21,119 +21,131 @@ sub minify_string {
     my $in_multi = 0;
     my $in_single = 0;
     my $in_string = 0;
+    my $in_comment = 0;
 
-    while ($input_string =~ m/"|(\/\*)|(\*\/)|(\/\/)|\n|\r/g)
+    while ($input_string =~ m/("|(\/\*)|(\*\/)|(\/\/)|[[:space:]])/gm)
     {
+        # Initialize context
+#        print "============================================================================\n";
+#        print "input_string='$input_string'\n";
         my $group = $1;
+#        print "group = '$group'\n";
         my $input_pos = pos $input_string;
-        my $group_len = defined $group ? length $group : 0;
-        if (!$in_multi || $in_single)
+#        print "input pos = $input_pos\n";
+        my $group_len = defined $group ? length $group : 1;
+#        print "group len = $group_len\n";
+#        print "index = $index\n";
+#        print "first if: in_multi=$in_multi in_single=$in_single in_string=$in_string in_comment=$in_comment\n";
+        $in_comment = $in_multi || $in_single;
+        # if not in a comment
+        if (!$in_comment)
         {
-            my $tmp = substr $input_string, 0, $input_pos;
+            # Get the sub string between previous pos and now pos
+            my $len = ($input_pos - $index - $group_len);
+#            print "    in first if: len = $len\n";
+            if ($len == -1) {$len = 0;}
+#            print "    in first if: after len = $len\n";
+            my $tmp = substr $input_string, $index, $len;
+#            print "    in first if: tmp = '$tmp'\n";
+            ## Eventually strip spaces
             if (! $in_string && $strip_space)
             {
-                $tmp =~ s/[[:space:]]+//g;
+                $tmp =~ s/[[:space:]]*//gm;
+#                print "    in first if: strip space: tmp = '$tmp'\n";
             }
+            # And add it in final result
             $new_str .= $tmp;
+#            print "    in first if: new_str = '$new_str'\n";
         }
+        # Else if not in comment and not stripping spaces
         elsif (! $strip_space)
         {
-            $new_str .= ' ' x (pos($input_string) - $index);
+            # we replace the substring with spaces
+            $new_str .= ' ' x ($input_pos - $index - $group_len);
+#            print "    in 2nd if: new_str = '$new_str'\n";
         }
-        $index = $input_pos + $group_len;
+#        print "after first if group: index=$index\n";
+        $index = $input_pos + $group_len - 1;
+#        print "after first if group: new index=$index\n";
         my $val = $group;
+        
+#        print "after first if group: val='$val'\n";
 
-        if ($val eq '"' && ! ($in_multi || $in_single))
+#        print "second if group: in_multi=$in_multi in_single=$in_single in_string=$in_string in_comment=$in_comment\n";
+#        print "val=='\\r' ?: ".($val eq "\r" ? "yes" : "no")."\n";
+#        print "val=='\\n' ?: ".($val eq "\n" ? "yes" : "no")."\n";
+#        print "val=='\\r\\n' ?: ".($val eq "\r\n" ? "yes" : "no")."\n";
+#        print "val=='\\n\\r' ?: ".($val eq "\n\r" ? "yes" : "no")."\n";
+        # If we are closing a string (in string, tok is a dbl quote and not in comment)
+        if ($val eq '"' && ! $in_comment)
         {
-            (my $escaped = $input_string) =~ m/(\\)*$/;
-            if (! $in_string || ($group_len % 2 == 0))
+            (my $escaped = substr( $input_string, $index, ($input_pos - $index - $group_len))) =~ m/(\\)*$/;
+            my $escaped_group = $1 || '';
+            my $escaped_group_len = length $escaped_group;
+#            print "    in 3rd if: escaped='$escaped'\n";
+#            print "    in 3rd if: escaped_group='$escaped_group'\n";
+#            print "    in 3rd if: escaped_group_len=$escaped_group_len\n";
+#            print "    in 3rd if: escaped_group_len %2 = ".($escaped_group_len % 2)."\n";
+            # We are either at start of string or unescaped dbl quote (end of string)
+            if (! $in_string || ! defined $1 || ($escaped_group_len % 2 == 0))
             {
-                $in_string = ! $in_string;
+#                print "    in if of 3rd if: in_string=$in_string\n";
+                $in_string = $in_string ? 0 : 1;
+#                print "    in if of 3rd if: new in_string=$in_string\n";
             }
-            $index--;
+            $new_str .= '"';
+#            print "    in if of 3rd if: new_str='$new_str'\n";
         }
-        elsif (! ($in_string || $in_multi || $in_single))
+        elsif (! ($in_string || $in_comment))
         {
-            if ($val eq '/*') {$in_multi = 1;}
-            elsif ($val eq '//') {$in_single = 1;}
+#            print "    in 4th if: val='$val'\n";
+            if ($val eq '/*') {$in_multi = 1; }
+            elsif ($val eq '//') {$in_single = 1; }
         }
         elsif ($val eq '*/' && $in_multi && !($in_string || $in_single))
         {
+#            print "    in 5th if: unset in_multi\n";
             $in_multi = 0;
-            if (! $strip_space) {$new_str .= ' ' x length($val);}
+            if (! $strip_space) 
+            {
+                $new_str .= ' ' x length($val);
+#                print "    in 5th if: new_str='$new_str'\n";
+            }
         }
-        elsif (($val eq '\r' || $val eq '\n') && ! ($in_multi || $in_string) && $in_single)
+        elsif (($val eq "\r" || $val eq "\n") && ! $in_multi && $in_single)
         {
+#            print "    in 6th if: unset in_single\n";
             $in_single = 0;
         }
-        elsif (! ($in_multi || $in_single) || 
-               (($val eq ' ' || $val eq '\r' || $val eq '\n' || $val eq '\t') && $strip_space))
+        elsif (! $in_comment || 
+               (($val eq ' ' || $val eq "\r" || $val eq "\n" || $val eq "\t") && !$strip_space))
         {
             $new_str .= $val;
+#            print "    in 7th if: new_str='$new_str'\n";
         }
+        $in_comment = $in_single || $in_multi;
 
         if (!$strip_space)
         {
+#            print "    in 8th if: val='$val'\n";
             if ($val eq '\r' || $val eq '\n')
             {
                 $new_str .= $val;
+#                print "    in 8th if: new_str='$new_str'\n";
             }
             elsif ($in_multi || $in_single)
             {
                 $new_str .= (' ' x length($val));
+#                print "    in 8th if: new_str='$new_str'\n";
             }
         }
+        
+#        print "new_str='$new_str'\n";
     }
 
+#    print "END: index=$index\n";
     $new_str .= substr $input_string, $index;
-    print "$new_str\n";
-}
-
-sub minify_file {
-    my $self = shift;
-    my $fi = shift;
-    my $fo = shift;
-
-    my $fin;
-    my $fout;
-    
-    open($fin, '<:encoding(UTF-8)', $fi)
-        or die "Cannot open file '$fi' for reading: $!";
-    open($fout, '>:encoding(UTF-8)', $fo)
-        or die "Cannot open file '$fo' for writing: $!";
-    my $oc = 0;
-    while (<$fin>) {
-        chomp;
-        s/\/\*([^*][^\/]|[^"])*\*\///g;
-        if (m/^([^\/][^*]|[^"])*\*\// && $oc == 1) { $oc = 0;}
-        if (m/\/\*([^*][^\/]|[^"])*$/ && $oc == 0) { $oc = 1;}
-        s/\/\*([^*][^\/]|[^"])*$//;
-        s/^([^*][^\/]|[^"])*\*\///;
-        s/\s*\/\/.*$//g;
-        s/\s+//g;
-        if ($oc == 0) {$fout->print("$_");}
-        
-    }
-}
-
-sub minify_handler {
-    my $self = shift;
-
-    my $fhi = shift;
-    my $fho = shift;
-    my $oc = 0;
-    while (<$fhi>) {
-        chomp;
-        s/\/\*([^*][^\/]|.)*\*\///g;
-        if (m/^([^\/][^*]|.)*\*\// && $oc == 1) { $oc = 0;}
-        if (m/\/\*([^*][^\/]|.)*$/ && $oc == 0) { $oc = 1;}
-        s/\/\*([^*][^\/]|.)*$//;
-        s/^([^*][^\/]|.)*\*\///;
-        s/\s*\/\/.*$//g;
-        s/\s+//g;
-        if ($oc == 0) {$fho->print("$_");}
-        
-    }
+#    print "END: substr input_string, index=".(substr $input_string, $index)."\n";
 }
 
 1;
--- json_minify/json-minify.pl
+++ json_minify/json-minify.pl
@@ -1,4 +1,4 @@
-#/usr/bin/perl
+#!/usr/bin/perl
 
 BEGIN{push @INC, '.'}
 
@@ -17,6 +17,45 @@ print "Processing test/test1.json ...";
 $minifier->minify_file("test/test1.json", "test/result1.json");
 system("diff test/expectedresult1.json test/result1.json");
 
-my $in_str = '{\n    // COMMENT\n    "key1": /* COMMENT */ "value1",\n    "key2": 4, /*COMMENT*/\n    "key3": false,\n    /* COMMENT\n     * COMMENT\n     * COMMENT */\n    "key4": /*\n    COMMENT\n */ [\n        "str1", "str2", "str3"\n    ],\n    "key5" : // COMMENT\n    {\n        "key51": "test ",\n        "key52": "test /* */ test "\n    }\n}\n';
-my $out_str = '{"key1":"value1","key2":4,"key3": false,"key4":["str1","str2","str3"],"key5":{"key51":"test ","key52":"test /* */ test "}}';
+my $in_str = "{\n    // COMMENT\n    \"key1\": /* COMMENT */ \"value1\",\n    \"key2\": 4, /*COMMENT*/\n    \"key3\": false,\n    /* COMMENT\n     * COMMENT\n     * COMMENT */\n    \"key4\": /*\n    COMMENT\n */ [\n        \"str1\", \"str2\", \"str3\"\n    ],\n    \"key5\" : // COMMENT\n    {\n        \"key51\": \"test \",\n        \"key52\": \"test /* */ test \"\n    }\n}\n";
+my $out_str = '{"key1":"value1","key2":4,"key3":false,"key4":["str1","str2","str3"],"key5":{"key51":"test ","key52":"test /* */ test "}}';
 my $ret_str = $minifier->minify_string($in_str);
+print "ret_str='$ret_str'\n";
+print "out_str='$out_str'\n";
+if ($out_str eq $ret_str)
+{
+    print "OK!\n";
+}
+else
+{
+    print "FAILED!\n";
+}
+$in_str = "\
+			// this is a JSON file with comments\n\
+			{\n\
+				\"foo\": \"bar\",	// this is cool\n\
+				\"bar\": [\n\
+					\"baz\", \"bum\", \"zam\"\n\
+				],\n\
+			/* the rest of this document is just fluff\n\
+			   in case you are interested. */\n\
+				\"something\": 10,\n\
+				\"else\": 20\n\
+			}\n\
+			\n\
+			/* NOTE: You can easily strip the whitespace and comments\n\
+			   from such a file with the JSON.minify() project hosted\n\
+			   here on github at http://github.com/getify/JSON.minify\n\
+    */\n";
+$out_str = "{\"foo\":\"bar\",\"bar\":[\"baz\",\"bum\",\"zam\"],\"something\":10,\"else\":20}";
+$ret_str = $minifier->minify_string($in_str);
+print "ret_str='$ret_str'\n";
+print "out_str='$out_str'\n";
+if ($out_str eq $ret_str)
+{
+    print "OK!\n";
+}
+else
+{
+    print "FAILED!\n";
+}
