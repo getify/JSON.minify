498ba7a0a0b8920642a08705ce749ca72c65b948
Rémi Cohen-Scali <remi@jayacode.fr>
2018-01-08 13:32:49 +0100

    Added implem for minify_string copied from the python algo

--- json_minify/JSON_minify.pm
+++ json_minify/JSON_minify.pm
@@ -11,6 +11,82 @@ sub new {
 }
 
 sub minify_string {
+    my $self = shift;
+    my $input_string = shift;
+    my $strip_space = shift || 1;
+
+    my $new_str = "";
+    my $index = 0;
+
+    my $in_multi = 0;
+    my $in_single = 0;
+    my $in_string = 0;
+
+    while ($input_string =~ m/"|(\/\*)|(\*\/)|(\/\/)|\n|\r/g)
+    {
+        my $group = $1;
+        my $input_pos = pos $input_string;
+        my $group_len = defined $group ? length $group : 0;
+        if (!$in_multi || $in_single)
+        {
+            my $tmp = substr $input_string, 0, $input_pos;
+            if (! $in_string && $strip_space)
+            {
+                $tmp =~ s/[[:space:]]+//g;
+            }
+            $new_str .= $tmp;
+        }
+        elsif (! $strip_space)
+        {
+            $new_str .= ' ' x (pos($input_string) - $index);
+        }
+        $index = $input_pos + $group_len;
+        my $val = $group;
+
+        if ($val eq '"' && ! ($in_multi || $in_single))
+        {
+            (my $escaped = $input_string) =~ m/(\\)*$/;
+            if (! $in_string || ($group_len % 2 == 0))
+            {
+                $in_string = ! $in_string;
+            }
+            $index--;
+        }
+        elsif (! ($in_string || $in_multi || $in_single))
+        {
+            if ($val eq '/*') {$in_multi = 1;}
+            elsif ($val eq '//') {$in_single = 1;}
+        }
+        elsif ($val eq '*/' && $in_multi && !($in_string || $in_single))
+        {
+            $in_multi = 0;
+            if (! $strip_space) {$new_str .= ' ' x length($val);}
+        }
+        elsif (($val eq '\r' || $val eq '\n') && ! ($in_multi || $in_string) && $in_single)
+        {
+            $in_single = 0;
+        }
+        elsif (! ($in_multi || $in_single) || 
+               (($val eq ' ' || $val eq '\r' || $val eq '\n' || $val eq '\t') && $strip_space))
+        {
+            $new_str .= $val;
+        }
+
+        if (!$strip_space)
+        {
+            if ($val eq '\r' || $val eq '\n')
+            {
+                $new_str .= $val;
+            }
+            elsif ($in_multi || $in_single)
+            {
+                $new_str .= (' ' x length($val));
+            }
+        }
+    }
+
+    $new_str .= substr $input_string, $index;
+    print "$new_str\n";
 }
 
 sub minify_file {
