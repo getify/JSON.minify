806b0df582ee10d488e59245f0a7728793090607
Rémi Cohen-Scali <remi@jayacode.fr>
2018-01-09 23:05:00 +0100

    Command script for json_minify
    Added very first implem for command script

--- json_minify/bin/json_minify.pl
+++ json_minify/bin/json_minify.pl
@@ -39,24 +39,121 @@
 
 our $VERSION = '1.0';
 
-
-
 BEGIN{push @INC, '/home/cohen/Sources/JSON-minify/json_minify/lib'}
 
 use strict;
 use warnings;
+use Getopt::Long;
+use Pod::Usage;
+use Data::Dumper qw(Dumper);
 use JSON_minify;
 
-my $fhi;
-open($fhi, "<:encoding(UTF-8)", $::main::ARGV[0])
-    or die "Error: Cannot open '".$::main::ARGV[0]."': $!\n";
-binmode $fhi;
-my $clang_tidy;
-while (<$fhi>)
-{
-    $clang_tidy .= $_;
-}
-close ($fhi);
+my $output = '-';
+my $help = 0;
+my $man = 0;
+my $strip = 0;
 
-my $minifier = JSON_minify->new();
-print $minifier->minify_string($clang_tidy, 0);
+GetOptions (
+    "output|o=s" => \$output, 
+    "help|h+"   => \$help, 
+    "man|m+"   => \$man, 
+    "strip|s!"  => \$strip)  
+    or pod2usage(2);  # flag
+pod2usage(1) if $help;
+pod2usage(-exitval => 0, -verbose => 2) if $man;
+
+sub minify_file {
+    my $fhi = shift;
+    my $fho = shift;
+    my $strip = shift;
+
+    binmode $fhi;
+    my $clang_tidy;
+    while (<$fhi>)
+    {
+        $clang_tidy .= $_;
+    }
+    close ($fhi);
+    
+    my $minifier = JSON_minify->new();
+    $fho->print($minifier->minify_string($clang_tidy, $strip));
+}
+
+my $fhi;
+my $fho;
+
+if ($output eq '-')
+{
+    $fho = \*STDOUT;
+}
+else
+{
+    open($fho, "<:encoding(UTF-8)", $output)
+        or die "Error: Cannot open '$output' for writing: $!\n";
+}
+
+Dumper(@ARGV);
+
+if (scalar @ARGV == 0)
+{
+    $fhi = \*STDIN;
+    minify_file($fhi, $fho, $strip);
+}
+else
+{
+    foreach my $fname (@ARGV)
+    {
+        open($fhi, "<:encoding(UTF-8)", $fname)
+            or warn "Error: Cannot open '$fname' for reading: $!\n";
+        minify_file($fhi, $fho, $strip);
+    }
+}
+
+__END__
+
+=head1 NAME
+
+    json_minify.pl - Command interface for JSON_minify module
+
+=head1 SYNOPSIS
+
+    json_minify.pl [options] [file [file ...]]
+
+    If no file is provided as option arguments, the strandard input
+    is used.
+    Options:
+       --output | -o <output-file>     output file
+       --strip | -s                    strip spaces
+       --help  | -h                    brief help message
+       --man  | -m                     full documentation message
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<--output|-o>
+
+    Specify the pathname for an output file.
+    If no output file is provided, result is sent
+    on standard output.
+
+=item B<--strip|-s>
+
+    Allows to strip spaces from the json content stream
+
+=item B<--help|-h>
+
+    Print a brief help message and exits.
+
+=item B<--man|-m>
+
+    Prints the manual page and exits.
+
+=back
+
+=head1 DESCRIPTION
+
+    B<JSON_minify.pl> will read the given input file(s) and will process it
+    in order to removed C:C++ comments.
+
+=cut
